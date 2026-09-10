d302f2ffcca4656c884675e3e82ce2a282209e08
mounttowntech <mounttowntech@gmail.com>
2026-09-10 12:46:27 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ const app = express();
 // MIDDLEWARE
 // ==========================================================
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
